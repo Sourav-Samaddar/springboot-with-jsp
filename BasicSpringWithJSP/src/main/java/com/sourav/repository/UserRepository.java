edcbad3cd03cc00b7528bb63f9eb4415b7f0d9ca
Sourav Samaddar <sourav.samaddar@jda.com>
2022-01-29 19:42:34 +0530

    first commit

--- BasicSpringWithJSP/src/main/java/com/sourav/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sourav.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sourav.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-	
-	public User findByUsernameAndPassword(String username,String password) ;
-}
